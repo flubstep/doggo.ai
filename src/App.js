3feb663cc3e3a3436d8f788565f752551b3f427c
Albert Sheu <albert.sheu@gmail.com>
2018-03-07 12:20:31 -0800

    Update styling and copy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from './doggo-square-logo.svg';
+import logo from './doggo-large-image.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Fetching wow intelligence for good doggos.</h1>
-          <h2 className="App-subtitle">Deep learning. Blockchains. Internet of Doge.</h2>
-        </header>
+          <h2 className="App-subtitle">Deep Learning. Blockchains. The Internet of Doge.</h2>
+        </div>
       </div>
     );
   }
